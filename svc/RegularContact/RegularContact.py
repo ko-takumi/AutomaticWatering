fad8e8028e9df5ef73aba9fe360a1a90eb236294
tanaka.takuya <taku-tanaka@tn-japan.co.jp>
2019-11-20 20:08:05 +0900

    add function

--- svc/RegularContact/RegularContact.py
+++ svc/RegularContact/RegularContact.py
@@ -2,6 +2,7 @@
 import Task
 import time
 from RegularContact.LINENotify import LINENotify
+from StateMemory.API_StateMemory import API_StateMemory
 
 
 class RegularContact(Task.Task):
@@ -16,9 +17,40 @@ class RegularContact(Task.Task):
 				print("RegularContact CMD: ", self.mNameSpace.popleft())
 
 			time.sleep(1)
+			
+			"水やり判定結果取得"
+			isWater, reason = StateMemory.API_StateMemory.getDisWater()
+			if reason == "温度":
+                            "湿度判定で水やった"
+                            print()
+                            RegularContact.LINENotify.execute()
+                        elif reason == "時間":
+			    "時間判定で水やった"
+			    print()
+			    RegularContact.LINENotify.execute()
+			elif reason == "手動":
+                            "手動判定で水やった"
+                            print()
+                            RegularContact.LINENotify.execute()
+                        else:
+                            "水やらなくていいよ"
+                            print()
+                            RegularContact.LINENotify.execute()
 
-	def func1(self):
-		pass
-
-	def stop(self):
-		pass
+                        "水量取得"
+			waterRmaining = StateMemory.API_StateMemory.getWaterRemaining()
+			if waterRmaining <= 50:
+                        			"水量が50%切った"
+                        			 print()
+                            RegularContact.LINENotify.execute()
+			elif waterRmaining == 0:
+                        			"水量が0%切った"
+                        			 print()
+                            RegularContact.LINENotify.execute()
+                        else:
+                            "水量は十分"
+                            print()
+                            RegularContact.LINENotify.execute()
+                            
+                        "土壌湿度取得"
+                        humidity = StateMemory.API_StateMemory.getHumidity()
