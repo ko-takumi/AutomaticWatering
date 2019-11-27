f2d17910cfb74e616701606f777b133abf48f858
tanaka.takuya <taku-tanaka@tn-japan.co.jp>
2019-11-27 19:46:24 +0900

    fix build error

--- svc/RegularContact/LINENotify/LINENotify.py
+++ svc/RegularContact/LINENotify/LINENotify.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
+import requests
 
 class LINENotify():
     def __init__(self):
+        
+        url = 'https://notify-api.line.me/api/notify'
+        access_token = 'Token'
+        headers = {'Authorization':'Bearer' + access_token}
+        
         pass
     
-    def execute():
+    def execute(self):
+        #message = 'hoge'
+        #payload = {'message':message}
+        #r = requests.post(url, headers=headers, params=payload,)
+        
         pass
--- svc/RegularContact/RegularContact.py
+++ svc/RegularContact/RegularContact.py
@@ -6,51 +6,56 @@ from StateMemory.API_StateMemory import API_StateMemory
 
 
 class RegularContact(Task.Task):
-	def __init__(self):
-		super(RegularContact, self).__init__()
+    mLINENotify		= None
+    mAPI_StateMemory	= None
+    
+    def __init__(self):
+        super(RegularContact, self).__init__()
+        self.mLINENotify = LINENotify.LINENotify()
+        self.mAPI_StateMemory = API_StateMemory()
 
-	def taskMain(self):
-		while(True):
-			print("RegularContact...")
+    def taskMain(self):
+        while(True):
+            print("RegularContact...")
 
-			if len(self.mNameSpace) > 0:
-				print("RegularContact CMD: ", self.mNameSpace.popleft())
+            if len(self.mNameSpace) > 0:
+                print("RegularContact CMD: ", self.mNameSpace.popleft())
 
-			time.sleep(1)
-			
-			"水やり判定結果取得"
-			isWater, reason = StateMemory.API_StateMemory.getDisWater()
-			if reason == "温度":
-                            "湿度判定で水やった"
-                            print()
-                            RegularContact.LINENotify.execute()
-                        elif reason == "時間":
-			    "時間判定で水やった"
-			    print()
-			    RegularContact.LINENotify.execute()
-			elif reason == "手動":
-                            "手動判定で水やった"
-                            print()
-                            RegularContact.LINENotify.execute()
-                        else:
-                            "水やらなくていいよ"
-                            print()
-                            RegularContact.LINENotify.execute()
+            time.sleep(1)
 
-                        "水量取得"
-			waterRmaining = StateMemory.API_StateMemory.getWaterRemaining()
-			if waterRmaining <= 50:
-                        			"水量が50%切った"
-                        			 print()
-                            RegularContact.LINENotify.execute()
-			elif waterRmaining == 0:
-                        			"水量が0%切った"
-                        			 print()
-                            RegularContact.LINENotify.execute()
-                        else:
-                            "水量は十分"
-                            print()
-                            RegularContact.LINENotify.execute()
-                            
-                        "土壌湿度取得"
-                        humidity = StateMemory.API_StateMemory.getHumidity()
+            #水やり判定結果取得
+            isWater, reason = self.mAPI_StateMemory.getDisWater()
+            if reason == 0:
+                #湿度判定で水やった
+                print()
+                self.mLINENotify.execute()
+            elif reason == 0:
+                #時間判定で水やった
+                print()
+                self.mLINENotify.execute()
+            elif reason == 0:
+                #手動判定で水やった
+                print()
+                self.mLINENotify.execute()
+            else:
+                #水やらなくていいよ
+                print()
+                self.mLINENotify.execute()
+
+            #水量取得
+            waterRmaining = self.mAPI_StateMemory.getWaterRemaining()
+            if waterRmaining <= 50:
+                #水量が50%切った
+                print()
+                self.mLINENotify.execute()
+            elif waterRmaining == 0:
+                #水量が0%切った
+                print()
+                self.mLINENotify.execute()
+            else:
+                #水量は十分
+                print()
+                self.mLINENotify.execute()
+
+            #土壌湿度取得
+            humidity = self.mAPI_StateMemory.getHumidity()
